ad64c504546e0116be319776bb17955f32f21407
mahadihasan2 <mahadihasan125643@gmail.com>
2022-08-28 00:42:20 +0600

    about section is done

---
+++ HTML/app.js
@@ -0,0 +1,14 @@
+var tablinks = document.getElementsByClassName("tablinks")
+var tabContents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link")
+    }
+    for (tabcontent of tabContents) {
+        tabcontent.classList.remove("active-tab")
+    }
+
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
